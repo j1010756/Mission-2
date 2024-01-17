c8e766e453c553afa6658f36af6b424af3c3a44c
j1010756 <106291794+j1010756@users.noreply.github.com>
2024-01-16 17:27:30 -0700

    Add project files.

---
+++ DiceRolls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class DiceRolls
+    {
+        public int[] Simulatation(int dice_rolls)
+        {
+            // make randomizer for numbers
+            Random randomizer = new Random();
+
+            // make array for results
+            int[] result_array = new int[11];    
+
+            // roll two dice each time and append result to the array
+            for (int i = 0; i < dice_rolls; i++)
+            {
+                int dice1 = randomizer.Next(1, 7);
+                int dice2 = randomizer.Next(1, 7);
+                int sum = dice1 + dice2;
+
+                result_array[sum - 2]++;
+            }
+
+            // return result
+            return result_array;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Mission2;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        // connect new class
+        DiceRolls dc = new DiceRolls();
+
+        // display initial prompts
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        // get number of dices rolls
+        int dice_rolls = int.Parse(System.Console.ReadLine());
+
+        // get array of results from other class
+        int[] results = dc.Simulatation(dice_rolls);
+
+        // display result messages
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS\nEach '*' represents 1% of the total number of rolls\nTotal number of rolls = " + dice_rolls);
+
+        // show results
+        ShowResults(results);
+
+        // display end message
+        System.Console.WriteLine("Thanks for using the simulator. Goodbye!");
+
+    }
+
+    public static void ShowResults(int[] results)
+    {
+        int totalCount = results.Sum(); // Calculate the total count of values
+
+        for (int i = 2; i <= 12; i++)
+        {
+            // write each number in the histogram
+            System.Console.Write($"{i,2}: ");
+
+            // determine percentage and write * to show it
+            int percentage = (int)Math.Round((double)results[i - 2] * 100 / totalCount);
+            for (int j = 0; j < percentage; j++)
+            {
+                System.Console.Write("*");
+            }
+
+            System.Console.WriteLine();
+        }
+    }
+
+}
